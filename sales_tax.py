1a60e0c326f27ce2c0c832f266902fe5ec435ff9
yawarkhan44 <yawarkhan44@yahoo.com>
2021-12-13 11:41:01 +0500

    Added some more test cases
    Added some more test cases to create a product and calculate the basic and import taxes that apply. Added the code to fix the test.

--- sales_tax.py
+++ sales_tax.py
@@ -1,7 +1,33 @@
 
 from math import ceil
 
-
+TAX_EXEMPTED_ITEMS = ["chocolate", "book", "pill", "chocolates", "books", "pills"]
 
 def calculate_rounded_tax(amount):
     return ceil(round(amount , 2) * 20) / 20
+class Product():
+    def __init__(self, item):
+        self.name = item["Name"]
+        self.price = item["Price"]
+        self.quantity = item["Quantity"]
+        self.imported = item["Imported"]
+        self.tax = self.calculate_tax()
+
+    def calculate_tax(self):
+        
+        exempted = False
+        salesTax = 0
+        importTax = 0
+
+        name = self.name.split()
+
+        for category in TAX_EXEMPTED_ITEMS:
+            if category in [x.lower() for x in name]:
+                exempted = True
+                break
+        if self.imported:
+            salesTax = calculate_rounded_tax(float(self.price) * 0.05)
+        if not exempted:
+            importTax = calculate_rounded_tax(float(self.price) * 0.1)
+        totalTax = salesTax + importTax
+        return totalTax
--- sales_tax_test.py
+++ sales_tax_test.py
@@ -9,6 +9,17 @@ class TestMiscellaneous(unittest.TestCase):
 
         for i in range(0, len(taxes)):
             self.assertEqual(sales_tax.calculate_rounded_tax(taxes[i]), roundedTaxes[i])
+            
+class TestProduct(unittest.TestCase):
+    def test_no_tax(self):
+        item = sales_tax.Product({"Name": "book", "Price": 12.49, "Quantity": 1, "Imported": False})
+        self.assertEqual(item.tax, 0)
+    def test_basic_tax(self):
+        item = sales_tax.Product({"Name": "music CD", "Price": 14.99, "Quantity": 1, "Imported": False})
+        self.assertEqual(item.tax, 1.5)
+    def test_basic_and_import_tax(self):
+        item = sales_tax.Product({"Name": "imported bottle of perfume", "Price": 47.50, "Quantity": 1, "Imported": True})
+        self.assertEqual(item.tax, 7.15)
 
 if __name__ == '__main__':
     unittest.main()
